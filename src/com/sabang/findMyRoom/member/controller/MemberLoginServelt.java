bffdb49f464457cd9c4dbf3b42ae1543d8bca24f
김태훈 <xogns603@hanmail.net>
2021-04-10 15:50:51 +0900

    feat : login 기능 추가
    로그인 기능 코드 추가

---
+++ src/com/sabang/findMyRoom/member/controller/MemberLoginServelt.java
@@ -0,0 +1,57 @@
+package com.sabang.findMyRoom.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.sabang.findMyRoom.member.model.dto.MemberDTO;
+import com.sabang.findMyRoom.member.model.service.MemberService;
+
+
+@WebServlet("/member/login")
+public class MemberLoginServelt extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String path = "/WEB-INF/views/member/signin.jsp";
+		
+		request.getRequestDispatcher(path).forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String userId = request.getParameter("userId");
+		String userPwd = request.getParameter("userPwd");
+		
+		System.out.println(userId);
+		System.out.println(userPwd);
+		
+		MemberDTO requestMember = new MemberDTO();
+		requestMember.setId(userId);
+		requestMember.setPwd(userPwd);
+		
+		MemberService memberService = new MemberService();
+		
+		MemberDTO loginMember = memberService.loginCheck(requestMember);
+//		System.out.println("로그인 회원 : " + loginMember);
+		
+		if(loginMember != null) {
+			HttpSession session = request.getSession();
+			session.setAttribute("loginMember", loginMember);
+			
+			response.sendRedirect(request.getContextPath()); // = /jsp 로 이동과 같은 의미
+		}else {
+			request.setAttribute("message", "로그인 실패!");
+			request.getRequestDispatcher("/WEB-INF/views/common/failed.jsp").forward(request, response);
+		}
+	}
+	
+
+}
--- src/com/sabang/findMyRoom/member/model/dao/MemberDAO.java
+++ src/com/sabang/findMyRoom/member/model/dao/MemberDAO.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -51,6 +52,71 @@ public class MemberDAO {
 		}
 		
 		return result;
+	}
+	
+	/* 암호화 처리 된 비밀번호 조회용 메소드 (로그인 확인용) */
+	public String selectEncryptedPwd(Connection con, MemberDTO requestMember) {
+
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		String encPwd = null;
+		
+		String query = prop.getProperty("selectEncryptedPwd");
+		
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, requestMember.getId());
+			
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				encPwd = rset.getString("USER_PWD");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			close(rset);
+			close(pstmt);
+		}
+		return encPwd;
+	}
+
+	public MemberDTO selectLoginMember(Connection con, MemberDTO requestMember) {
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		MemberDTO loginMember = null;
+		
+		String query = prop.getProperty("selectLoginMember");
+		
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, requestMember.getId());
+			
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				loginMember = new MemberDTO();
+				loginMember.setNo(rset.getInt("USER_NO"));
+				loginMember.setId(rset.getString("USER_ID"));
+				loginMember.setNickname(rset.getString("NICKNAME"));
+				loginMember.setPhone(rset.getString("PHONE"));
+				loginMember.setEmail(rset.getString("EMAIL"));
+				loginMember.setEnrollDate(rset.getDate("ENROLL_DATE"));
+				loginMember.setRole(rset.getString("USER_ROLE"));
+				loginMember.setStatus(rset.getString("USER_STATUS"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			close(rset);
+			close(pstmt);
+		}
+		return loginMember;
 	}
 
 }
