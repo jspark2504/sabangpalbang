baaaa39d2fd48f7af8143fd4841f86f51fbe5de0
JH <yoojh0219@gmail.com>
2021-04-20 22:06:49 +0900

    feat : 지도 마커 위 인포윈도우 올리는 작업 중

--- web/resources/js/map.js
+++ web/resources/js/map.js
@@ -52,6 +52,18 @@ for (var i = 0 ; i < count ; i++) {
 
 				/* 지도에 마커 표시 */
 				marker.setMap(map);
+
+				var iwContent = '<div style="padding:5px;">' + formatPrice +'<br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    				iwPosition = coords; //인포윈도우 표시 위치입니다
+
+				// 인포윈도우를 생성합니다
+				var infowindow = new kakao.maps.InfoWindow({
+				    position : iwPosition,
+				    content : iwContent
+				});
+
+				// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+				infowindow.open(map, marker);
 			}
 	});
 }
